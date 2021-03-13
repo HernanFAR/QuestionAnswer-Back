fdb9c6e9361f87b44c8fb5a5c0bbcbe8b672823f
Hernan Alvarez <halvarez@Austranet.com>
2021-03-13 00:50:50 -0300

    Se agregaron los valores default de Category

---
+++ Repository/Migrations/20210313035015_InitialCatalog_CategoryData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class InitialCatalog_CategoryData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedAt", "LastUpdated", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 3, 13, 0, 50, 14, 724, DateTimeKind.Local).AddTicks(3969), new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(6440), "Vida" },
+                    { 2, new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7961), new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7974), "Juegos" },
+                    { 3, new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7978), new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7981), "Amor" },
+                    { 4, new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7983), new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7986), "Dolores" },
+                    { 5, new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7988), new DateTime(2021, 3, 13, 0, 50, 14, 726, DateTimeKind.Local).AddTicks(7991), "Miselaneo" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
--- Repository/QuestionAnswerContext.cs
+++ Repository/QuestionAnswerContext.cs
@@ -10,13 +10,30 @@ namespace Repository
 {
     public class QuestionAnswerContext : IdentityDbContext<QuestionAnswerUser, IdentityRole, string>
     {
-        public QuestionAnswerContext(DbContextOptions<QuestionAnswerContext> options) : 
-            base(options) { }
+        public QuestionAnswerContext(DbContextOptions<QuestionAnswerContext> options) :
+            base(options)
+        { }
 
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Question> Questions { get; set; }
 
         public DbSet<Answer> Answers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>(e => 
+            {
+                e.HasData(
+                    new Category { Id = 1, Name = "Vida" }, 
+                    new Category { Id = 2, Name = "Juegos" }, 
+                    new Category { Id = 3, Name = "Amor" }, 
+                    new Category { Id = 4, Name = "Dolores" }, 
+                    new Category { Id = 5, Name = "Miselaneo" }
+                );
+            });
+        }
     }
 }
