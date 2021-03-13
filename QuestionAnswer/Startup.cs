b31a2fefde8653aa1743f4553b366c91d20ce9f3
Hernan Alvarez <halvarez@Austranet.com>
2021-03-13 11:43:44 -0300

    Se elimino JwtConfiguration de la API y se uso el de service

--- QuestionAnswer/Startup.cs
+++ QuestionAnswer/Startup.cs
@@ -1,27 +1,21 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using QuestionAnswer.Models;
 using Repository;
 using Repository.Models;
 using Service;
 using Service.Interfaces;
+using Service.Models;
 using Service.Profile;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace QuestionAnswer
 {
