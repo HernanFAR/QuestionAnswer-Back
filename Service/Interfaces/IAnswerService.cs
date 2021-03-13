48e83cb7242560fb92af6ac5f9e72737c47e6dfb
Hernan Alvarez <halvarez@Austranet.com>
2021-03-13 01:13:15 -0300

    Se agregaron las interfaces de AnswerService, CategoryService y QuestionService

---
+++ Service/Interfaces/IAnswerService.cs
@@ -0,0 +1,13 @@
+﻿using Quicker.Interfaces.Service;
+using Repository.DTO;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Interfaces
+{
+    public interface IAnswerService : IFullServiceAsync<int, Answer, AnswerDTO>
+    {
+    }
+}
---
+++ Service/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using Quicker.Interfaces.Service;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Interfaces
+{
+    public interface ICategoryService : IFullServiceAsync<int, Category>
+    {
+    }
+}
